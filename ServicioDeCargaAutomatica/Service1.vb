47aee4485e3ef53610a51ab60b0680f67534868e
Desarrollo_Software <CasReportes@gmail.com>
2019-05-13 15:12:18 -0500

    Agregar archivos de proyecto.

---
+++ ServicioDeCargaAutomatica/Service1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Service1
+
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        ' Agregue el código aquí para iniciar el servicio. Este método debería poner
+        ' en movimiento los elementos para que el servicio pueda funcionar.
+    End Sub
+
+    Protected Overrides Sub OnStop()
+        ' Agregue el código aquí para realizar cualquier anulación necesaria para detener el servicio.
+    End Sub
+
+End Class
